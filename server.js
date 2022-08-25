96e6f592829057cd2d3a030e3b526b3b23ca4462
Alan Chelko <achelko@gmail.com>
2022-08-25 02:43:49 +0200

    Update

--- server.js
+++ server.js
@@ -2,39 +2,26 @@
 
 console.log('Gledaj!  First server!');
 
-// REQUIRES - we use require on the backend and import on the front end
-const express = require('express');
 
-//required for .env -- need the config method -- do not set to a variable like for express...
+// REQUIRED
+const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const axios = require('axios');
 
 //DATA
 let dataPet = require('./data/pets.json');
-let data = require('./data/weather.json');
+// let data = require('./data/weather.json');
 
-
-
-// const cors = require('cors');
-
-// once express is in we need to use it - per express docs - this is the server
-// console.log ran got a resonse after npm start, but immediately shut down; need a port to keep it up
+//Express instance
 const app = express();
-
-// // middleware to share resources across the internet
-// app.use(cors());
-
-// define my port for my server to listen on
-// // 3002 — if my server runs on 3002, I know something is wrong with my .env file or how I'm importing the values from it
 const PORT = process.env.PORT || 3002;
+app.use(cors());
 
+//////////////////////////////////
 
-// // ROUTES - our endpoints, like locationiq search...
+//Endpoints
 
-// // Base route - proof of life
-// .get() is an express method
-// // it correlates to axios.get
-// // the first parameter is a URL in quote,
-// // the second is a callback function
 app.get('/', (request, response) => {
   console.log('This base route is showing up in the terminal!');
   response.status(200).send('Welcome to our server');
@@ -47,26 +34,84 @@ app.get('/hello', (request, response) => {
   response.status(200).send(`Hello ${firstName} ${lastName} from the hello route!`);
 });
 
-app.get('/pet', (request, response) => {
-  let species = request.query.species;
-  console.log(species);
-  // let dataToSend = dataPet.find(pet => pet.species === species);
-  let dataToGroom = dataPet.find(pet => pet.species === species);
-  let dataToSend = new Pet(dataToGroom);
-  response.status(200).send(dataToSend);
-  // response.status(200).send('you requested a species');
+app.get('/pet', (request, response, next) => {
+  try {
+    let species = request.query.species;
+    console.log(species);
+    // let dataToSend = dataPet.find(pet => pet.species === species);
+    let dataToGroom = dataPet.find(pet => pet.species === species);
+    let dataToSend = new Pet(dataToGroom);
+    response.status(200).send(dataToSend);
+    // response.status(200).send('you requested a species');
+  } catch (error) {
+    next(error);
+  }
 });
 
 
-app.get('/weather', (request, response) => {
-  let city_name = request.query.city_name;
+///Adam function code
+// app.get('/photos', getPhotos);
+
+// async function getPhotos(request, response) {
+//     // note the .searchQuery
+//     const searchQuery = request.query.searchQuery;
+//     const url = `https://api.unsplash.com/search/photos/?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=${searchQuery}`;
+//     try {
+//         // axios get request to our api
+//         const photosResponse = await axios.get(url);
+//         // console.log(photosResponse);
+//         // groomed data
+//         const photoArray = photosResponse.data.results.map(photo => new Photo(photo));
+//         response.status(200).send(photoArray);
+//     } catch (err) {
+//         console.log('error message is: ', err);
+//         response.status(500).send(`server error`);
+//     }
+// }
+
+///AC new weather code
+app.get('/weather', getWeather);
+
+async function getWeather(request, response) {
+  const city_name = request.query.city_name;
   console.log(city_name);
-  // let dataToSend = data.find(weather => weather.city_name === city_name);
-  let dataToGroom = data.find(weather => weather.city_name === city_name);
-  let dataToSend = new Weather(dataToGroom);
-  // response.status(200).send('you requested a city');
-  response.status(200).send(dataToSend);
-});
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHERBIT_API_KEY}&query=${city_name}`;
+  try {
+      const weatherResponse = await axios.get(url);
+      console.log(weatherResponse);
+
+      const dataToSend = weatherResponse.data.map(object => {
+      return new Forecast(object);
+      });
+
+      response.status(200).send(dataToSend);
+
+  } catch (error) {
+    console.log('error message is: ', err);
+    response.status(500).send(`server error`);
+  }
+
+};
+
+
+
+// ///AC original weather code
+// app.get('/weather', (request, response, next) => {
+//   try {
+//     let city_name = request.query.city_name;
+//     console.log(city_name);
+//     // let dataToSend = data.find(weather => weather.city_name === city_name);
+//     let dataToGroom = data.find(weather => weather.city_name === city_name);
+//     let dataToSend = dataToGroom.data.map(object => {
+//       return new Forecast(object);
+//     });
+//     // let dataToSend = new Weather(dataToGroom);
+//     // response.status(200).s-end('you requested a city');
+//     response.status(200).send(dataToSend);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // app.get('/pet', (request, response, next) => {
 //   try {
@@ -88,6 +133,16 @@ class Pet {
   }
 }
 
+class Forecast {
+	constructor(weatherObj) {
+	 this.date = weatherObj.valid_date;	
+	 this.description = weatherObj.weather.description;
+
+ }
+}
+
+
+
 class Weather {
   constructor(weatherObj){
     this.lat = weatherObj.lat;
@@ -102,11 +157,9 @@ app.get('*', (request, response) => {
 
 // // ERRORS
 // // Handle any errors
-// app.use((error, request, response, next)=> {
-  //   response.status(500).send(error.message);
-  // });
+app.use((error, request, response, next)=> {
+  response.status(500).send(error.message);
+});
   
-  // // LISTEN
-  // // starts the server
-  // // .listen() is an express method that takes in a PORT value and a callback function
+  
   app.listen(PORT, ()=> console.log(`We are up on PORT: ${PORT}`));
