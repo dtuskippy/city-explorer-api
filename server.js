6b8a512092f4f5361d7e625fe3d0e9c3340a528a
Alan Chelko <achelko@gmail.com>
2022-08-27 01:19:25 +0200

    Minor edit

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ async function getWeather(request, response, next) {
   console.log('lat', lat);
   console.log('lon', lon);
   const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHERBIT_API_KEY}`;
+  console.log(url);
   // const url = http://api.weatherbit.io/v2.0/forecast/daily?key=9e38424d9f774ef3995a3244ca4c1a11&lat=38.123&lon=-78.543
   //let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
   
